1b5387ae93184bf4b2af3a6b74423cb44db7041e
Slick AI <mahermah1976@gmail.com>
2025-06-05 13:12:21 +0300

    Implement FeaturedProjects

--- client/src/components/FeaturedProjects.tsx
+++ client/src/components/FeaturedProjects.tsx
@@ -1,3 +1,26 @@
+import '@styles/components/FeaturedProjects.scss'
+import { PROJECTS } from '@const'
+
 export default function FeaturedProjects() {
-    return <></>
+    return (
+        <section id='featured-projects'>
+            <h2 className='section-title'>Featured Projects</h2>
+            <div className='project-grid'>
+                {PROJECTS.map(project => (
+                    <a href={project.link} key={project.title} className='project-card'>
+                        <img src={project.img} alt={project.title} className='project-image'/>
+                        <div className='project-content'>
+                            <h3 className='project-title'>{project.title}</h3>
+                            <p className='project-description'>{project.description}</p>
+                            <div className='tech-stack'>
+                                {project.stack.map(tech => (
+                                    <span className='tech' key={tech}>{tech}</span>
+                                ))}
+                            </div>
+                        </div>
+                    </a>
+                ))}
+            </div>
+        </section>
+    )
 }
